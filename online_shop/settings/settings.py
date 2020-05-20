844fbf868b33afc585fd82a8d097bcc4706e301b
jonndoe <qb89@yandex.ru>
2020-05-20 23:58:15 +0300

    added cart app

--- online_shop/settings/settings.py
+++ online_shop/settings/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'online_shop.cart.context_processors.cart',
             ],
         },
     },
