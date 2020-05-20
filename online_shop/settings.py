fd7a2eabc345616b5246e87128dfa702e24cbf2b
jonndoe <qb89@yandex.ru>
2020-05-20 16:50:43 +0300

    added categories and product models

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'online_shop.shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
